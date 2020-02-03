fa478b9d064689a0a1c1c84631756b9dbf7388dc
Wesley Jones <wes@upgradeya.com>
2020-02-03 18:16:21 -0500

    Adding the ability to copy/paste directly from local clipboard.

--- index.js
+++ index.js
@@ -323,4 +323,30 @@ function debounce(func, delay) {
   };
 }
 
+var IMAGE_MIME_REGEX = /^image\/(p?jpeg|gif|png)$/i;
+var loadImage = function (file) {
+    var reader = new FileReader();
+    reader.onload = function(e){
+        var img = document.createElement('img');
+        img.src = e.target.result;
+        var range = window.getSelection().getRangeAt(0);
+        range.deleteContents();
+        range.insertNode(img);
+    };
+    reader.readAsDataURL(file);
+};
+
+document.onpaste = function(e){
+    var items = e.clipboardData.items;
+
+    for (var i = 0; i < items.length; i++) {
+        if (IMAGE_MIME_REGEX.test(items[i].type)) {
+            loadImage(items[i].getAsFile());
+            return;
+        }
+    }
+
+    // Normal paste handling here
+}
+
 test();
