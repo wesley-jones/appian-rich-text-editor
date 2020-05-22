bac4afef8baffcd6a38a32beb824969f90935d62
Wesley Jones <wes@upgradeya.com>
2020-05-21 23:36:02 -0400

    Adding initial image support code to Quill.

--- index.js
+++ index.js
@@ -153,6 +153,13 @@ function test() {
         // updateValue();
       }
     });
+
+    // quill editor add image handler
+    quill.getModule('toolbar').addHandler('image', () => {
+      selectLocalImage();
+    });
+
+
   }
 
   /* Update maxSize if specified */
@@ -322,6 +329,67 @@ function debounce(func, delay) {
     }, delay);
   };
 }
+
+
+/**
+ * Step1. select local image
+ *
+ */
+function selectLocalImage() {
+  const input = document.createElement('input');
+  input.setAttribute('type', 'file');
+  input.click();
+
+  // Listen upload local image and save to server
+  input.onchange = () => {
+    const file = input.files[0];
+
+    // file type is only image.
+    if (/^image\//.test(file.type)) {
+      saveToServer(file);
+    } else {
+      console.warn('You could only upload images.');
+    }
+  };
+}
+
+/**
+ * Step2. save to server
+ *
+ * @param {File} file
+ */
+function saveToServer(file=File) {
+  const fd = new FormData();
+  fd.append('image', file);
+
+  const xhr = new XMLHttpRequest();
+  xhr.open('POST', 'https://srademo.appiancloud.com/suite/webapi/upload-image', true);
+  xhr.setRequestHeader("Origin", 'srademo.appiancloud.com');
+  xhr.onload = () => {
+    if (xhr.status === 200) {
+      // this is callback data: url
+      const url = JSON.parse(xhr.responseText).data;
+      insertToEditor(url);
+    }
+  };
+  xhr.send(fd);
+}
+
+/**
+ * Step3. insert image url to rich editor.
+ *
+ * @param {string} url
+ */
+function insertToEditor(url=string) {
+  // push image url to rich editor.
+  const range = editor.getSelection();
+  editor.insertEmbed(range.index, 'image', `https://srademo.appiancloud.com/docs/${url}`);
+}
+
+
+
+
+
 
 /**
  * Enable copy/paste from clipboard for non-html images.
